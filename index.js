80a1e6ab940983f4761c8930e5863da1a6d59ee6
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-12-24 23:56:49 +0600

    Added menu and review data into mongodb

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d0o34gr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(process.env.DB_USER, process.env.DB_PASS);
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -28,6 +26,22 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        /* All Collections */
+        const menuCollection = client.db("savoryStoriesDB").collection("menu");
+        const reviewCollection = client.db("savoryStoriesDB").collection("reviews");
+
+        /* For Menu */
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        });
+
+        /* For Review */
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -39,7 +53,6 @@ async function run() {
 run().catch(console.dir);
 
 /* Connect to Mongodb end */
-
 
 
 
